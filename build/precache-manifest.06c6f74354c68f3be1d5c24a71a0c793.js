f4aaded70598db65c68ab78fc12e0337952ea18a
Ilya <ilyabolshakov44@gmail.com>
2021-03-26 14:25:02 +0300

    creating build

---
+++ build/precache-manifest.06c6f74354c68f3be1d5c24a71a0c793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2028ca6f8eccfc1e80e",
+    "url": "/static/css/main.e246b632.chunk.css"
+  },
+  {
+    "revision": "f2028ca6f8eccfc1e80e",
+    "url": "/static/js/main.f2028ca6.chunk.js"
+  },
+  {
+    "revision": "b55884c336f37565bcd4",
+    "url": "/static/css/1.e54dcca4.chunk.css"
+  },
+  {
+    "revision": "b55884c336f37565bcd4",
+    "url": "/static/js/1.b55884c3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dea39c69a3cb972e0283881c12179849",
+    "url": "/index.html"
+  }
+];
